c75107d7ee99e0d2e7332105ac6669fc802e8f21
Balázs Varga <balazsvarga91@gmail.com>
2024-01-24 20:12:18 +0100

    feat: add new launch

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,11 +1,13 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
-    flightNumber: 1,
+    flightNumber: 100,
     mission: 'mission1',
     rocket: 'rocket',
     launchDate: new Date(),
-    destination: 'destination',
+    target: 'destination',
     customer: [
         'customer1',
         'customer2'
@@ -20,6 +22,20 @@ function getAllLaunches() {
     return Array.from(launches.values());
 }
 
+function addNewLaunch(launch) {
+    latestFlightNumber++;
+    launches.set(
+        latestFlightNumber,
+        Object.assign(launch, {
+            customer: ['NASA'],
+            upcoming: true,
+            success: true,
+            flightNumber: latestFlightNumber
+     }
+    ));
+}
+
 module.exports = {
-    getAllLaunches
+    getAllLaunches,
+    addNewLaunch,
 }
