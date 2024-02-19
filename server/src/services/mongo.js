3796feec0c5eb22902fd3131729852af7bd3f4ef
Balázs Varga <balazsvarga91@gmail.com>
2024-02-19 13:32:24 +0100

    feat: use dotenv

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL = 'mongodb+srv://nasa-api:urSOrZJNisK9w9YG@nasacluster.otjexxz.mongodb.net/nasa?retryWrites=true&w=majority';
+require('dotenv').config();
+
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
     console.log('MongoDB connection ready!');
